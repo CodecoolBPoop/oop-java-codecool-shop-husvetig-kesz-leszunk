1151412265880408ac90e18aa8d34d47a0db5839
peterSzalma <petiszalma@gmail.com>
2018-12-06 22:46:14 +0100

    checkout page done

--- src/main/java/com/codecool/shop/controller/CheckoutController.java
+++ src/main/java/com/codecool/shop/controller/CheckoutController.java
@@ -2,13 +2,14 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.model.BaseModel;
 import com.codecool.shop.model.Product;
-import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.ShoppingCart;
-import com.codecool.shop.model.Supplier;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -18,12 +19,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
-@WebServlet(urlPatterns = {"/shopping-cart"})
-public class ShoppingCartController extends HttpServlet implements ProductDao {
+@WebServlet(urlPatterns = "/checkout")
+public class CheckoutController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -40,37 +38,8 @@ public class ShoppingCartController extends HttpServlet implements ProductDao {
         context.setVariable("recipient", "World");
         context.setVariable("category", productCategoryDataStore.find(1));
         context.setVariable("product", ShoppingCart.shoppingCartList);
+        context.setVariable("totalPrice", ShoppingCart.totalPrice());
 
-        engine.process("shopping-cart.html", context, resp.getWriter());
-    }
-
-    @Override
-    public void add(Product product) {
-
-    }
-
-    @Override
-    public Product find(int id) {
-        return null;
-    }
-
-    @Override
-    public void remove(int id) {
-
-    }
-
-    @Override
-    public List<Product> getAll() {
-        return null;
-    }
-
-    @Override
-    public List<Product> getBy(Supplier supplier) {
-        return null;
-    }
-
-    @Override
-    public List<Product> getBy(ProductCategory productCategory) {
-        return null;
+        engine.process("checkout.html", context, resp.getWriter());
     }
 }
--- src/main/java/com/codecool/shop/model/ShoppingCart.java
+++ src/main/java/com/codecool/shop/model/ShoppingCart.java
@@ -1,15 +1,28 @@
 package com.codecool.shop.model;
 
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class ShoppingCart extends Product{
+public class ShoppingCart{
     
 
     public static List<Product> shoppingCartList = new ArrayList<>();
 
+    private static float totalPrice;
+
 
     public ShoppingCart(String name, float defaultPrice, String currencyString, String description, ProductCategory productCategory, Supplier supplier) {
-        super(name, defaultPrice, currencyString, description, productCategory, supplier);
+        //super(name, defaultPrice, currencyString, description, productCategory, supplier);
+    }
+
+    public static float totalPrice() {
+        for (int i = 0; i<shoppingCartList.size(); i++) {
+            totalPrice += shoppingCartList.get(i).getDefaultPrice();
+        }
+
+        return totalPrice;
     }
 }
