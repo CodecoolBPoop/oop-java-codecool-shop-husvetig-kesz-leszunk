ed985d5e482b9e8a5b94a06754e23b9e3db5f08c
Imre Maczkó <immaczko@gmail.com>
2018-12-06 13:34:24 +0100

    some product added, header font changed

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -34,14 +34,17 @@ public class Initializer implements ServletContextListener {
         productCategoryDataStore.add(tablet);
         ProductCategory cellphone = new ProductCategory("Cellphone", "Hardware", "A cellphone, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
         productCategoryDataStore.add(cellphone);
-        ProductCategory spaceShipPart = new ProductCategory("Spaceship part", "Hardware", "A cellphone, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
+        ProductCategory spaceShipPart = new ProductCategory("Spaceship", "Hardware", "A cellphone, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
         productCategoryDataStore.add(spaceShipPart);
+        ProductCategory weapon = new ProductCategory("Weapon", "Weapon", "A cellphone, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
+        productCategoryDataStore.add(weapon);
 
         //setting up products and printing it
-        productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
+        productDataStore.add(new Product("Laser Pistol", 999.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", weapon, amazon));
         productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
         productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
-        productDataStore.add(new Product("Spaceship Engine", 12000, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", spaceShipPart, amazon));
+        productDataStore.add(new Product("Spaceship Engine", 11990, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", spaceShipPart, amazon));
+        productDataStore.add(new Product("Hyper drive", 8980, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", spaceShipPart, lenovo));
 
     }
 }
