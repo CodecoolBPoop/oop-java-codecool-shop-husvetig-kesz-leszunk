9ae617a830cebffe2eb86122612e3dafe87858c9
peterSzalma <petiszalma@gmail.com>
2019-01-03 13:17:15 +0100

    delete some useless row

--- src/main/sql/init_db.sql
+++ src/main/sql/init_db.sql
@@ -46,13 +46,3 @@ INSERT INTO product (id, name, default_price, description, category_id, supplier
 values (1, 'S-5 heavy blaster pistol', 1499.9, 'A versatile weapon with a heavy wooden gripstock and twin scopes for sighting and rangefinding', 3, 1),
        (2, 'Bionic Arm', 2490.9, 'An affordable, advanced and intuitive bionic arm. Adjustable finger bone crack sound effect. Grab it fast.', 1, 1),
        (3, 'Custom Build Podracer', 5990, 'A one-man cockpit that pulled along by two engines. Capable of achieving speeds over 900 kilometers per hour, sometimes fails at start.', 2, 2);
-
-
-
-
-"S-5 heavy blaster pistol", 1499.9f, "USD", "A versatile weapon with a heavy wooden gripstock and twin scopes for sighting and rangefinding. ", weapon, amazon));
-productDataStore.add(new Product("Bionic Arm", 2490.9f, "USD", "An affordable, advanced and intuitive bionic arm. Adjustable finger bone crack sound effect. Grab it fast.", robotics, lenovo));
-productDataStore.add(new Product("Custom Build Podracer", 5990, "USD", "A one-man cockpit that pulled along by two engines. Capable of achieving speeds over 900 kilometers per hour, sometimes fails at start.", spaceShipPart, amazon));
-productDataStore.add(new Product("Spaceship Engine", 11990, "USD", "Spaceship engine at it's finest. Good condition, fall in price.", spaceShipPart, amazon));
-productDataStore.add(new Product("T-14 hyperdrive generator", 8990, "USD", "A propulsion system that allows a starship to reach lightspeed.", spaceShipPart, amazon));
-productDataStore.add(new Product("Pit Droid", 2990, "USD", "Cheap and durable repair droids. Tap their 'nose' to fold into compact form. ", robotics, lenovo));
