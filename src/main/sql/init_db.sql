9922160d1e802ac43ea78cb04388cac6b3bb0a21
peterSzalma <petiszalma@gmail.com>
2019-01-03 15:21:13 +0100

    get all supplier

--- src/main/sql/init_db.sql
+++ src/main/sql/init_db.sql
@@ -36,11 +36,11 @@ values ('Nubian-Design-Collective', 'Digital content and services'),
        ('Serv-O-Droids', 'Droids');
 
 INSERT INTO product_category(id, name, department, description)
-values (1, 'Robotics', 'Hardware', 'Robotics'),
-       (2, 'Spaceship', 'Hardware', 'Spaceships and spaceship parts'),
-       (3, 'Weapon', 'Weapon', 'Weapons and stuff' );
+values ('Robotics', 'Hardware', 'Robotics'),
+       ('Spaceship', 'Hardware', 'Spaceships and spaceship parts'),
+       ('Weapon', 'Weapon', 'Weapons and stuff' );
 
 INSERT INTO product (id, name, default_price, description, category_id, supplier_id)
-values (1, 'S-5 heavy blaster pistol', 1499.9, 'A versatile weapon with a heavy wooden gripstock and twin scopes for sighting and rangefinding', 3, 1),
-       (2, 'Bionic Arm', 2490.9, 'An affordable, advanced and intuitive bionic arm. Adjustable finger bone crack sound effect. Grab it fast.', 1, 1),
-       (3, 'Custom Build Podracer', 5990, 'A one-man cockpit that pulled along by two engines. Capable of achieving speeds over 900 kilometers per hour, sometimes fails at start.', 2, 2);
+values ('S-5 heavy blaster pistol', 1499.9, 'A versatile weapon with a heavy wooden gripstock and twin scopes for sighting and rangefinding', 3, 1),
+       ('Bionic Arm', 2490.9, 'An affordable, advanced and intuitive bionic arm. Adjustable finger bone crack sound effect. Grab it fast.', 1, 1),
+       ('Custom Build Podracer', 5990, 'A one-man cockpit that pulled along by two engines. Capable of achieving speeds over 900 kilometers per hour, sometimes fails at start.', 2, 2);
